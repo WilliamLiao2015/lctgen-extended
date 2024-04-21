b9d13d59cb6443df48cec2ef2a69c724f1b5be91
William Liao <williamliao2015@gmail.com>
2024-04-21 22:12:24 +0800

    Update colab setup with comments

--- setup/colab.py
+++ setup/colab.py
@@ -7,8 +7,12 @@ def setup_colab():
 
     if not in_colab: raise Exception("This function is only needed in Google Colab.")
 
-    # Google Colab
+    # Google Colab secrets
     from google.colab import userdata
+
+    # Set up the Google Generative AI authentification
     genai.configure(api_key=userdata.get("GOOGLE_API_KEY"))
+
+    # Set up the OpenAI authentification
     openai.organization = userdata.get("OPENAI_ORGANIZATION")
     openai.api_key = userdata.get("OPENAI_API_KEY")
