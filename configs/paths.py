ac842bc71e6aeadc093dec2e6efc22a0ee3e071f
William Liao <williamliao2015@gmail.com>
2024-04-21 23:39:36 +0800

    Update lctgen_dir path in configs/paths.py

--- configs/paths.py
+++ configs/paths.py
@@ -5,5 +5,5 @@ if in_colab: base_dir = "/content"
 else: base_dir = "."
 
 
-lctgen_dir = f"{base_dir}/lctgen"
+lctgen_dir = f"{base_dir}/lctgen-extended/lctgen"
 prompts_dir = f"{lctgen_dir}/lctgen/gpt/prompts/attr_ind_motion"
