a4be98affe79d2e0147a325174feb5401b4d756c
William Liao <williamliao2015@gmail.com>
2024-04-22 01:54:04 +0800

    Fix wrong config file path

--- llms/openai.py
+++ llms/openai.py
@@ -1,9 +1,9 @@
 from imports.lctgen import get_config, registry
-from configs.paths import prompts_dir
+from configs.paths import lctgen_dir
 
 
 def get_openai_llm(llm_model_name="gpt-3.5-turbo"):
-    llm_cfg = get_config(f"{prompts_dir}/non_api_cot_attr_20m.yaml")
+    llm_cfg = get_config(f"{lctgen_dir}/lctgen/gpt/cfgs/attr_ind_motion/non_api_cot_attr_20m.yaml")
     llm_cfg.merge_from_list(["LLM.CODEX.MODEL", llm_model_name])
 
     llm_model = registry.get_llm("codex")(llm_cfg)
