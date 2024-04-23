3d78b3365ac2ef7a2e1426636985192b1685fc81
William Liao <williamliao2015@gmail.com>
2024-04-23 22:31:35 +0800

    Add llama3.py

---
+++ llms/llama3.py
@@ -0,0 +1,28 @@
+from configs.paths import prompts_dir
+from imports.system import json
+from imports.packages import requests
+
+
+def inference_llama3_llm(query):
+    with open(f"{prompts_dir}/sys_non_api_cot_attr_20m.prompt") as fp:
+        system_prompt = fp.read()
+
+    with open(f"{prompts_dir}/non_api_cot_attr_20m.prompt") as fp:
+        user_prompt = fp.read()
+
+    prompt = prompt.replace("INSERT_QUERY_HERE", query)
+    response = requests.post("https://fumes-api.onrender.com/llama3", json={
+        "prompt": json.dumps({
+            "systemPrompt": system_prompt, 
+            "user": user_prompt
+        }),
+        "temperature":0.75,
+        "topP":0.9,
+        "maxTokens": 1024
+    }, stream=True)
+
+    for chunk in response.iter_content(chunk_size=1024):  
+        if chunk:
+            llm_result += chunk.decode("utf-8")
+
+    return llm_result
