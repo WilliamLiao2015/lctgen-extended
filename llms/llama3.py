4bcb0cbc44fecca22656e7a75decd22b3a3081d1
William Liao <williamliao2015@gmail.com>
2024-04-23 23:09:19 +0800

    Refactor inference_llama3_llm function in llama3.py to support disable streaming

--- llms/llama3.py
+++ llms/llama3.py
@@ -3,14 +3,14 @@ from imports.system import json
 from imports.packages import requests
 
 
-def inference_llama3_llm(query):
+def inference_llama3_llm(query, stream=False):
     with open(f"{prompts_dir}/sys_non_api_cot_attr_20m.prompt", "r", encoding="utf-8") as fp:
         system_prompt = fp.read()
 
     with open(f"{prompts_dir}/non_api_cot_attr_20m.prompt", "r", encoding="utf-8") as fp:
         user_prompt = fp.read()
 
-    prompt = prompt.replace("INSERT_QUERY_HERE", query)
+    user_prompt = user_prompt.replace("INSERT_QUERY_HERE", query)
     response = requests.post("https://fumes-api.onrender.com/llama3", json={
         "prompt": json.dumps({
             "systemPrompt": system_prompt, 
@@ -19,10 +19,15 @@ def inference_llama3_llm(query):
         "temperature":0.75,
         "topP":0.9,
         "maxTokens": 1024
-    }, stream=True)
+    }, stream=stream)
 
-    for chunk in response.iter_content(chunk_size=1024):  
-        if chunk:
-            llm_result += chunk.decode("utf-8")
+    llm_result = ""
+
+    if stream:
+        for chunk in response.iter_content(chunk_size=1024):  
+            if chunk:
+                llm_result += chunk.decode("utf-8")
+    else:
+        llm_result = response.text
 
     return llm_result
